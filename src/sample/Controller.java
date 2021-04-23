8c1c34b3106fb13687d8fb3cf2ef6b147da5e14d
madaOprea <61909649+madaOprea@users.noreply.github.com>
2021-04-24 02:00:41 +0300

    basic form

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,30 @@
-package sample;
+package src.sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
 
 public class Controller {
+    @FXML
+    public Button loginButton;
+    @FXML
+    public TextField usernameTextField;
+    @FXML
+    public TextField passwordTextField;
+
+    public void loginButtonOnAction(){
+        System.out.println("pressed");
+        loginButton.setText("Hey baby");
+    }
+
+    public void handleCancelAction(ActionEvent actionEvent){
+        System.out.println("pressed");
+        Stage stage = (Stage) loginButton.getScene().getWindow();
+        stage.close();
+
+        loginButton.setText("Hey baby");
+    }
 }
