12ee253aa42a8c31c97c00aed2b1958d341bc1d4
madaOprea <61909649+madaOprea@users.noreply.github.com>
2021-04-24 16:19:22 +0300

    basic login

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class Controller {
+    @FXML
+    public Button loginButton;
+    @FXML
+    public TextField usernameTextField;
+    @FXML
+    public TextField passwordTextField;
+
+    public void loginButtonOnAction(ActionEvent actionEvent){
+        System.out.println("pressed");
+        if (usernameTextField.getText().trim().isEmpty()) {
+            System.out.println("-1-");
+        } else {
+            System.out.println("-2-");
+        }
+
+        loginButton.setText("Hey baby");
+    }
 }
